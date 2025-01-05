d0dd82dc9bc635efebeb7b910b23c18772f3010f
balraj hanmanthugari <balraj.hanmanthugari@gmail.com>
2025-01-06 00:36:49 +0530

    crud operations

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,8 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 function EmployeeForm() {
+    const params = useParams();
+    const [state, setState] = useState({
+        id: '',
+        name: '',
+        email: '',
+        mobile: ''
+    });
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        //state.id=params.id;
+        if(params.id) {
+            axios.get('http://localhost:8080/student/'+params.id)
+            .then((res) => {
+                console.log(res);
+                setState(res.data.student);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    }, [params]);
+    
+    const submitEmployee = (event) => {
+        event.preventDefault();
+        if(params.id) {
+            axios.put('http://localhost:8080/student/'+params.id, state)
+            .then((res) => {
+                console.log(res);
+                navigate("/employees", { replace: true });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+        else {
+            axios.post('http://localhost:8080/student', state)
+            .then((res) => {
+                console.log(res);
+                navigate('/employees', { replace: true });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    };
+
     return (
-        <div>Employee Form</div>
+        <form>
+            <div className="form-group">
+                <label htmlFor="formControlInput1">ID</label>
+                <input type="id" className="form-control" value={state.id} onChange={(e)=>setState({...state, id: e.target.value})} id="formControlInput1" placeholder="ID" disabled={Number(params.id)>0}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="formControlInput1">Name</label>
+                <input type="name" className="form-control" value={state.name} onChange={(e)=>setState({...state, name: e.target.value})} id="formControlInput1" placeholder="Name"/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="formControlInput2">Mobile</label>
+                <input type="email" className="form-control" value={state.email} onChange={(e)=>setState({...state, email: e.target.value})} id="formControlInput2" placeholder="name@example.com" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="formControlInput3">Email</label>
+                <input type="mobile" className="form-control" value={state.mobile} onChange={(e)=>setState({...state, mobile: e.target.value})} id="formControlInput3" placeholder="9876543210"/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="formControlSelect2">Example multiple select</label>
+                <select multiple className="form-control" id="formControlSelect2">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label htmlFor="formControlTextarea1">Text Area</label>
+                <textarea className="form-control" id="formControlTextarea1" rows="3"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={ submitEmployee }>Submit</button>
+        </form>
     )
 }
 
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,26 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function EmployeeList() {
-    const [state, setState] = useState([{
-        name: 'ada',
-        email: 'asd@as.com',
-        mobile: '12213123'
-    }, {
-        name: 'adsaa',
-        email: 'asadd@as.com',
-        mobile: '126783'
-    }]);
+    const [state, setState] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/student?name=e')
+            .then((res) => {
+                setState(res.data.students);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        //setState([{ "id": 1, "name": "emp1", "mobile": 9959453851, "email": "emp1@gmail.com" }, { "id": 3, "name": "em3", "mobile": 9959453853, "email": "emp3@gmail.com" }, { "id": 5, "name": "empp5", "mobile": 9959453855, "email": "emp5@gmail.com" }, { "id": 7, "name": "emp7", "mobile": 9959453857, "email": "emp7@gmail.com" }, { "id": 9, "name": "empstu9", "mobile": 9959453859, "email": "emp9@gmail.com" }]);
+    }, []);
+
+    const updateEmployee = (id) => {
+        navigate('/employees/'+id);
+    };
+
+    const deleteEmployee = (id) => {
+        axios.delete('http://localhost:8080/student/'+id)
+            .then((res) => {
+                console.log(res);
+                setState(
+                    state.filter((item) => {
+                        return item.id !== Number(id);
+                    })
+                );
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     return (
         <>
             <div className="card">
                 <div className="card-header">
                     Employee List
+                    <Link to="/employees/add" className="btn btn-primary text-right">Create Employee</Link>
                 </div>
                 <div className="card-body">
                     <table className="table table-striped">
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Mobile</th>
@@ -28,18 +55,22 @@ function EmployeeList() {
                             </tr>
                         </thead>
                         <tbody>
-                        {
-                            state?.map((emp) => {
-                                return (
-                                <tr>
-                                    <td>{emp?.name}</td>
-                                    <td>{emp?.email}</td>
-                                    <td>{emp?.mobile}</td>
-                                    <td ></td>
-                                </tr>
-                                )
-                            })
-                        }
+                            {
+                                state?.map((emp) => {
+                                    return (
+                                        <tr>
+                                            <td>{emp?.id}</td>
+                                            <td>{emp?.name}</td>
+                                            <td>{emp?.email}</td>
+                                            <td>{emp?.mobile}</td>
+                                            <td>
+                                                <button onClick={ ()=>{updateEmployee(emp?.id)} } className="btn btn-info btn-sm btn-primary me-2">Edit</button>
+                                                <button className="btn btn-info btn-sm btn-danger" onClick={ () => deleteEmployee(emp?.id) }>Delete</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
